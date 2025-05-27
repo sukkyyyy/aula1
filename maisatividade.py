641722f3bfff9395eaa07f6c56d29de3b482b118
sukkyyyy <aliagamatheus5@gmail.com>
2025-05-27 09:07:41 -0300

    Add files via upload

---
+++ maisatividade.py
@@ -0,0 +1,8 @@
+temperatura = float(input("Digite a temperatura: "))
+
+if temperatura > 30:
+    print("Sensação Térmica: Está muito quente!!!")
+elif temperatura >= 15:
+    print("Sensação Térmica: Temperatura agradável.")
+else:
+    print("Sensação Térmica: Está frio!")
